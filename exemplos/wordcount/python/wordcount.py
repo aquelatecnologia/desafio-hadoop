89e3acf253935b23e1c483c6bbc242fe06a2b01f
Prof. Bruno Almeida <bruno@aquelatecnologia.com.br>
2018-10-09 11:19:36 -0300

    corrigindo name

--- exemplos/wordcount/python/wordcount.py
+++ exemplos/wordcount/python/wordcount.py
@@ -4,8 +4,8 @@ from mrjob.step import MRStep
 class WordCount(MRJob):
     def steps(self):
         return [
-            MRStep(mapper=self.mapper,
-                   reducer=self.reducer)
+            MRStep(mapper=self.mapper, reducer=self.reducer),
+            MRStep(mapper=self.sort_mapper, reducer=self.sort_reducer)
         ]
 
     def mapper(self, _, line):
@@ -15,6 +15,12 @@ class WordCount(MRJob):
 
     def reducer(self, key, values):
         yield key, sum(values)
+
+    def sort_mapper(self, word, count):
+        yield "%05d" % count, word
+
+    def sort_reducer(self, count, words):
+        yield count, " , ".join(words)
 
 if __name__ == '__main__':
     WordCount.run()
